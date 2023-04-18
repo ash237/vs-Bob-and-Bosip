fdd383667f92565d7e7de0f500bf7fd26ed63a99
HMFNF <51531073+hoaanhmine@users.noreply.github.com>
2023-04-18 17:16:50 +0700

    update source/PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5760,7 +5760,7 @@ class PlayState extends MusicBeatState
 
 					if (SONG.validScore)
 					{
-						NGio.unlockMedal(60961);
+						//NGio.unlockMedal(60961);
 						Highscore.saveWeekScore(storyWeek, campaignScore, storyDifficulty);
 					}
 
